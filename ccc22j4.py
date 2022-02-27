af62d308ed0769b4ce47643e86dd37e62b5c56ff
Danny Wang (ThePeeps191) <74725787+ThePeeps191@users.noreply.github.com>
2022-02-27 09:17:34 -0700

    code

--- ccc22j4.py
+++ ccc22j4.py
@@ -1,1 +1,18 @@
-
+X = int(input())
+must = [input().split() for _ in range(X)]
+Y = int(input())
+mustnot = [input().split() for _ in range(Y)]
+G = int(input())
+groups = [input().split() for _ in range(G)]
+count = 0
+violatedmust, violatedmustnot = [], []
+for group in groups:
+	for m in must:
+		if (m[0] not in group and m[1] in group or m[0] in group and m[1] not in group) and m not in violatedmust:
+			count += 1
+			violatedmust.append(m)
+	for m in mustnot:
+		if (m[0] in group and m[1] in group) and m not in violatedmustnot:
+			count += 1
+			violatedmustnot.append(m)
+print(count)
