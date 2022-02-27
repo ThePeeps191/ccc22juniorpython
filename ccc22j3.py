5f262902e76368c543b110435140158e07feece2
Danny Wang (ThePeeps191) <74725787+ThePeeps191@users.noreply.github.com>
2022-02-27 09:17:05 -0700

    code

--- ccc22j3.py
+++ ccc22j3.py
@@ -1,1 +1,23 @@
+from string import ascii_uppercase as u
+from string import digits as d
 
+instructions = input()
+output = ""
+last_digit = False
+for c in instructions:
+	if c in u and not last_digit:
+		output += c
+		last_digit = False
+	elif c in u and last_digit:
+		output += "\n" + c
+		last_digit = False
+	elif c == "+":
+		output += " tighten "
+		last_digit = False
+	elif c == "-":
+		output += " loosen "
+		last_digit = False
+	elif c in d:
+		last_digit = True
+		output += c
+print(output)
