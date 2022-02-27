a8fa3e0fe7d0f8ecf5922da9a24a1c424e42215d
Danny Wang (ThePeeps191) <74725787+ThePeeps191@users.noreply.github.com>
2022-02-27 09:16:36 -0700

    code

--- ccc22j2.py
+++ ccc22j2.py
@@ -1,1 +1,11 @@
-
+N = int(input())
+members = []
+for _ in range(N):
+	a, b = int(input()), int(input())
+	score = 5 * a - 3 * b
+	members.append(score)
+c = 0
+for m in members:
+	if m > 40:
+		c += 1
+print(f"{c}{'+' if len(members) == c else ''}")
